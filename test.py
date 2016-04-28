2649beb9a6a13fc67b007aa261ebef853adefa55
lizitong <lizitong99@gmail.com>
2016-04-29 05:24:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ tip_amount			tolls_amount			improvement_surcharge	total_amount
 """
 from pyspark import SparkConf, SparkContext, SparkFiles
 from operator import add
-#from shapely.geometry import MultiPoint, Point, Polygon
 import shapefile
 
 APP_NAME = "Traffic_Peak_Analysis"
@@ -71,27 +70,43 @@ def parse(line):
 		x2 = float(line[9])
 		y2 = float(line[10])
   	except:
-  		print "*********************"
-  		print "Invalid Point, line is:", line
-  		return ("Invalid Point", 1)
-	county1 = "Not found"
-	county2 = "Not found"
+  		pass
+
+	point1 = (round(x1, 3), round(y1, 3))
+	point2 = (round(x2, 3), round(y2, 3))
+	newLine = [time1, time2, point1, point2]
+	#newLine = (time + "," + county, 1)
+	return newLine
+
+def findCounty(point):
+	county = "Not found"
 	srs = shapeRecs.value
 	for sr in srs:
 		coords = sr.shape.points
 		bbox = sr.shape.bbox
-		if point_inside_polygon(x1,y1,coords, bbox):
-			county1 = sr.record[6]
-		if point_inside_polygon(x2,y2,coords, bbox):
-			county2 = sr.record[6]
-		
-	if county1 == "Not found":
-		county1 = "Richmond"
-	if county2 == "Not found":
-		county2 = "Richmond"
-	newLine = [time1, time2, county1, county2]
-	#newLine = (time + "," + county, 1)
+		if point_inside_polygon(point[0],point[1],coords, bbox):
+			county = sr.record[2]
+			break
+	return county
+
+def mapLocations(line):
+	point = line[0]
+	county = findCounty(point)
+	if county == "Not found": county = "Richmond"
+	newLine = (county, line[1])
 	return newLine
+
+def mapRoutes(line):
+	trip = line[0]
+	point1 = trip[1]
+	point2 = trip[2]
+	county1 = findCounty(point1)
+	if county1 == "Not found": county1 = "Richmond"
+	county2 = findCounty(point2)
+	if county2 == "Not found": county2 = "Richmond"
+	newLine = ((trip[0],county1,county2), line[1])
+	return newLine
+
 
 def deleteInvalidLines(line):
 	try:
@@ -101,15 +116,41 @@ def deleteInvalidLines(line):
 	return line[9] != "0.0" and line[10] != "0.0"
 
 def main(sc):
-	trips = sc.textFile("/taxidata/green/").map(lambda line:line.split(",")).filter(deleteInvalidLines)
-	parsedTrips = trips.map(parse)
-	parsedTrips.cache()
-	busyLocations = parsedTrips.flatMap(lambda line:line[2:4]).map(lambda x:(x,1))
-	bysyTimes = parsedTrips.flatMap(lambda line:line[0:2]).map(lambda x:(x,1))
-	busyRoutes = parsedTrips.map(lambda line:(line[0]+","+line[2]+","+line[3], 1))
-	busyLocations.reduceByKey(add).sortBy(lambda x:x[1], False).saveAsTextFile("Busy_Locations")
-	bysyTimes.reduceByKey(add).sortBy(lambda x:x[1], False).saveAsTextFile("Busy_Times")
-	busyRoutes.reduceByKey(add).sortBy(lambda x:x[1], False).saveAsTextFile("Busy_Trips")
+
+	csvPaths = []
+	csvPaths.append("/taxidata/yellow/yellow_tripdata_2015-01.csv")
+	"""
+	for i in range(1, 13):
+		if i < 10:
+			month = "0"+str(i)
+		else: month = str(i)
+		#green_tripdata_2015-01.csv
+		path = "/taxidata/green/green_tripdata_2015-" + month + ".csv"
+		csvPaths.append(path)
+		path2 = "/taxidata/yellow/yellow_tripdata_2015-" + month + ".csv"
+		csvPaths.append(path2)
+	"""
+
+	totalTimes = sc.emptyRDD()
+	totalLocations = sc.emptyRDD()
+	totalRoutes = sc.emptyRDD()
+	for path in csvPaths:
+		trips = sc.textFile(path).map(lambda line:line.split(",")).filter(deleteInvalidLines)
+		parsedTrips = trips.map(parse)
+		parsedTrips.cache()
+		busyTimes = parsedTrips.flatMap(lambda line:line[0:2]).map(lambda x:(x,1)).reduceByKey(add)
+		locations = parsedTrips.flatMap(lambda line:line[2:4]).map(lambda x:(x,1)).reduceByKey(add)
+		busyLocations = locations.map(mapLocations).reduceByKey(add)
+		routes = parsedTrips.map(lambda line:((line[0],line[2],line[3]), 1)).reduceByKey(add)
+		busyRoutes = routes.map(mapRoutes).reduceByKey(add)
+
+		totalTimes = totalTimes.union(busyTimes)
+		totalLocations = totalLocations.union(busyLocations)
+		totalRoutes = totalRoutes.union(busyRoutes)
+
+	totalTimes.reduceByKey(add).sortBy(lambda x:x[1], False).saveAsTextFile("Busy_Times")
+	totalLocations.reduceByKey(add).sortBy(lambda x:x[1], False).saveAsTextFile("Busy_Locations")
+	totalRoutes.reduceByKey(add).sortBy(lambda x:x[1], False).saveAsTextFile("Busy_Routes")
 
 if __name__ == "__main__":
 	conf = SparkConf()
@@ -117,7 +158,7 @@ if __name__ == "__main__":
 	conf.setMaster('yarn-client')
 	sc = SparkContext(conf=conf)
 	sc.addPyFile("shapefile.py")
-	shapefilePath = "../NY_counties_clip/NY_counties_clip"
+	shapefilePath = "../newShp/cty036"
 	sf = shapefile.Reader(shapefilePath)
 	shapeRecords = sf.shapeRecords()
 	shapeRecs = sc.broadcast(shapeRecords)
